1f400f56ffb48dc01d96b9db5b3904e33570aaf0
Golak Prasad Jena <jena.golak@gmail.com>
2018-03-14 15:33:36 +0530

    Atticus Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
+import { OurPhilosophyComponent } from './our-philosophy/our-philosophy.component';
+import { OurCriteriaComponent } from './our-criteria/our-criteria.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'who-we-are', component: WhoWeAreComponent},
+  {path:'our-philosophy', component: OurPhilosophyComponent},
+  {path:'our-criteria', component: OurCriteriaComponent},
+  {path:'investments', component: InvestmentsComponent},
+  {path:'career', component: CareerComponent},
+  {path:'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    TopbarComponent,
+    WhoWeAreComponent,
+    OurPhilosophyComponent,
+    OurCriteriaComponent,
+    InvestmentsComponent,
+    CareerComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
